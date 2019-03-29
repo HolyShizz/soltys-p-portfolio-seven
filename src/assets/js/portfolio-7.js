2a1cf6b00b6f38bae0a06ea749daee9a9ea1a459
Holyshizz <filippsoltys@gmail.com>
2019-03-29 22:57:59 +0200

    subscription form (mdc button text-field+bs4grid)

--- src/assets/js/portfolio-7.js
+++ src/assets/js/portfolio-7.js
@@ -1,5 +1,6 @@
+    //jquery for slick-carousel
 import $ from "jquery";
-
+    //initializing slick-carousel
 $(document).ready(function(){
     $('.slider').slick({
         slidesToShow: 1,
@@ -11,11 +12,20 @@ $(document).ready(function(){
         prevArrow: '.arrow-down',
         nextArrow: '.arrow-up',
         dots: true,
+        appendDots: '.slider-dots-container'
       });
   });   
     //Material components
+    import {MDCRipple} from '@material/ripple';
 import {MDCTopAppBar} from '@material/top-app-bar/index';
-
+import {MDCTextField} from '@material/textfield';
 // Instantiation
 const topAppBarElement = document.querySelector('.mdc-top-app-bar');
 const topAppBar = new MDCTopAppBar(topAppBarElement);
+//const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
+    return new MDCTextField(el);
+  });
+const buttonRipple = [].map.call(document.querySelectorAll('.mdc-button'), function(el) {
+    return new MDCRipple(el);
+  });
