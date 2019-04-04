e7785088ed1cd5844042f19b6c36ec6e5db31145
Holyshizz <filippsoltys@gmail.com>
2019-04-04 15:08:49 +0300

    mdc-card with YT iframe added

--- src/assets/js/portfolio-7.js
+++ src/assets/js/portfolio-7.js
@@ -1,32 +1,44 @@
     //jquery for slick-carousel
-import $ from "jquery";
-    //initializing slick-carousel
-$(document).ready(function(){
-    $('.slider').slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        vertical: true,
-        verticalSwiping: true,
-        zIndex: 1,
-        arrows: true,
-        prevArrow: '.arrow-down',
-        nextArrow: '.arrow-up',
-        dots: true,
-        appendDots: '.slider-dots-container'
-      });
-  }); 
-  //Material components
-import {MDCRipple} from '@material/ripple';
-const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action';
-const ripples = [].map.call(document.querySelectorAll(selector), function(el) {
-  return new MDCRipple(el);
-});
-import {MDCTopAppBar} from '@material/top-app-bar/index';
-const topAppBarElement = document.querySelector('.mdc-top-app-bar');
-const topAppBar = new MDCTopAppBar(topAppBarElement);
-topAppBar.unbounded = true;
-import {MDCTextField} from '@material/textfield';
-const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function(el) {
-    return new MDCTextField(el);
-  });
+    import $ from "jquery";
 
+
+    //initializing slick-carousel
+    $(document).ready(function () {
+        $('.slider').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            vertical: true,
+            verticalSwiping: true,
+            zIndex: 1,
+            arrows: true,
+            prevArrow: '.arrow-down',
+            nextArrow: '.arrow-up',
+            dots: true,
+            appendDots: '.slider-dots-container'
+        });
+    });
+
+
+    //Material components
+    import {
+        MDCRipple
+    } from '@material/ripple';
+    const selector = '.mdc-button, .mdc-icon-button, .mdc-card__primary-action, .mdc-card__primary-action--dark';
+    const ripples = [].map.call(document.querySelectorAll(selector), function (el) {
+        return new MDCRipple(el);
+    });
+    import {
+        MDCTopAppBar
+    } from '@material/top-app-bar/index';
+    const topAppBarElement = document.querySelector('.mdc-top-app-bar');
+    const topAppBar = new MDCTopAppBar(topAppBarElement);
+    topAppBar.unbounded = true;
+    import {
+        MDCTextField
+    } from '@material/textfield';
+    const textField = [].map.call(document.querySelectorAll('.mdc-text-field'), function (el) {
+        return new MDCTextField(el);
+    });
+
+
+    
